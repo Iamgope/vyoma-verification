f85028624945fbe7016f89bae7eafdbed67cbb04
Aman Gope <65496184+Iamgope@users.noreply.github.com>
2023-03-11 23:09:57 +0530

    Update adder.v

--- demo/adder/adder.v
+++ demo/adder/adder.v
@@ -1,5 +1,12 @@
-// See LICENSE.vyoma for more details
 
 module adder(a, b, sum);
 
- 
+  input [3:0] a, b;
+  output reg [4:0] sum;
+
+  always @(a or b) 
+  begin
+    sum = a + b;
+  end
+
+endmodule : adder
